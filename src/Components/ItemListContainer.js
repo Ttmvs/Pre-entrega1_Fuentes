49ff7f741781a6b7e31494b873c2cc27735dd540
Tomás <tomasfuentesm45@gmail.com>
2023-07-28 14:42:16 -0400

    se anadieron clases y estilos a imagenes

--- src/Components/ItemListContainer.js
+++ src/Components/ItemListContainer.js
@@ -6,7 +6,7 @@ function Item (props){
   return(
     <>
     <h4>{props.name}</h4>
-    <p>${props.value}</p>
+    <p className='flow-text'>${props.value}</p>
     </>
   );
 }
@@ -15,9 +15,15 @@ function ItemListContainer() {
     <>
     <div className='container'>
       <div className='row'>
-        <div className='col s4 center-align'><Item name="Hoodie The Last Winter Full Zip" value="47.990"></Item></div>
-        <div className='col s4 center-align' ><Item name="JEANS BAGGY BLACK WASHED" value="38.990"></Item></div>
-        <div className='col s4 center-align'><Item name="TEE THE LAST WINTER" WASHED value="25.000"></Item></div>
+        <div className='col s3 center-align'><Item name="Hoodie The Last Winter Full Zip" value="47.990"></Item>
+          <img src={Hoodie} className='col s10'/> 
+          </div>
+        <div className='col s4 center-align' ><Item name="JEANS BAGGY BLACK WASHED" value="38.990"></Item>
+        <img src={Jeans} className='col s8'/>
+        </div>
+        <div className='col s4 center-align'><Item name="TEE THE LAST WINTER" WASHED value="25.000"></Item>
+        <img src={Tshirt} className='col s8' />
+        </div>
       </div>
     </div>
    </>
@@ -25,5 +31,6 @@ function ItemListContainer() {
   
   )
 }
+
 
 export default ItemListContainer
