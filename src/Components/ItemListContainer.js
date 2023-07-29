2e481ad1ab4ea312c31365048226cf1b6c4d6a3d
Tomás <tomasfuentesm45@gmail.com>
2023-07-28 22:41:29 -0400

    prueba de estilos

--- src/Components/ItemListContainer.js
+++ src/Components/ItemListContainer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Hoodie from "../Assets/hoodie_the_las_winter.png"
 import Jeans from "../Assets/jeans_baggy_black.png"
 import Tshirt from "../Assets/tee_the_last_winter.png"
+import SobreNosotros from './SobreNosotros'
 function Item (props){
   return(
     <>
@@ -16,19 +17,19 @@ function ItemListContainer() {
     <div className='container'>
       <div className='row'>
         <div className='col s3 center-align'><Item name="Hoodie The Last Winter Full Zip" value="47.990"></Item>
-          <img src={Hoodie} className='col s10'/> 
+          
           </div>
-        <div className='col s4 center-align' ><Item name="JEANS BAGGY BLACK WASHED" value="38.990"></Item>
-        <img src={Jeans} className='col s8'/>
+          <div className='col s4 center-align' ><Item name="JEANS BAGGY BLACK WASHED" value="38.990"></Item>
+          
         </div>
-        <div className='col s4 center-align'><Item name="TEE THE LAST WINTER" WASHED value="25.000"></Item>
-        <img src={Tshirt} className='col s8' />
+          <div className='col s4 center-align'><Item name="TEE THE LAST WINTER" WASHED value="25.000"></Item>
+        
         </div>
       </div>
     </div>
    </>
 
-  
+
   )
 }
 
--- src/Components/SobreNosotros.js
+++ src/Components/SobreNosotros.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Hoodie from "../Assets/hoodie_the_las_winter.png"
+import Jeans from "../Assets/jeans_baggy_black.png"
+import Tshirt from "../Assets/tee_the_last_winter.png"
 
 function SobreNosotros() {
   return (
-    <h1>HOLA</h1>
+    <img src={Hoodie} alt="" />
   )
 }
 
